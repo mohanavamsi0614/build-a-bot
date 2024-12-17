ac78f1b69af5789e08c15b0bf619d76556be8279
mohanavamsi0614 <mohanavamsi14@gmail.com>
2024-12-17 16:45:17 +0530

    final

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
                 <path d="M12 1c6.075 0 11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1ZM2.5 12a9.5 9.5 0 0 0 9.5 9.5 9.5 9.5 0 0 0 9.5-9.5A9.5 9.5 0 0 0 12 2.5 9.5 9.5 0 0 0 2.5 12Z"></path>
               </svg>
             </div>
-            <div className="text-2xl ml-1">Date & Time: To Be Announced</div>
+            <div className="text-2xl ml-1">Date & Time: 28 Dec 2024</div>
           </div>
           <div className="flex justify-evenly items-center">
             <div className="icon" aria-label="Event Location">
